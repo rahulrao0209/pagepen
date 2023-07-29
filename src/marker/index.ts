cd33eb00271962edf68a19dc6510a08710e84ddd
Rahul Rao <rahulrao0209@gmail.com>
2023-07-29 22:35:09 +0530

    Pagepen | Add experimental walkTree method to enable selections across ranges

--- src/marker/index.ts
+++ src/marker/index.ts
@@ -14,17 +14,79 @@ export default class Marker {
     const range = node.getRangeAt(0);
     const content = range.cloneContents();
 
-    /** Delete the range. */
-    range.deleteContents();
+    if (range.startContainer === range.endContainer) {
+      /** Delete the range. */
+      range.deleteContents();
 
-    /** Append a new node with the range content */
-    const replacement = document.createElement("span");
-    replacement.append(content);
-    replacement.id = uid;
-    replacement.classList.add(feature, "cursor-pointer");
-    this.#marked.set(uid, replacement);
-    range.insertNode(replacement);
-    node.collapseToEnd();
+      /** Append a new node with the range content */
+      const replacement = document.createElement("span");
+      replacement.append(content);
+      replacement.id = uid;
+      replacement.classList.add(feature, "cursor-pointer");
+      this.#marked.set(uid, replacement);
+      range.insertNode(replacement);
+      node.collapseToEnd();
+    } else this.walkTree(range);
+  }
+
+  walkTree(range: Range) {
+    const startContainer = range.startContainer;
+    const endContainer = range.endContainer;
+
+    /** Use a tree walker */
+    const walker = document.createTreeWalker(
+      range.commonAncestorContainer || document,
+      NodeFilter.SHOW_TEXT | NodeFilter.SHOW_ELEMENT
+    );
+
+    let currentNode: Node | null = walker.nextNode();
+
+    while (currentNode) {
+      console.log(`
+        name: ${currentNode.nodeName}
+        text: ${currentNode.textContent}
+      `);
+      if (
+        currentNode.nodeType === Node.TEXT_NODE &&
+        (currentNode === startContainer || currentNode === endContainer)
+      ) {
+        const startOffset =
+          startContainer === currentNode ? range.startOffset : 0;
+        const endOffset =
+          endContainer === currentNode
+            ? range.endOffset
+            : currentNode.textContent?.length;
+
+        const selectedText = currentNode.textContent
+          ?.slice(startOffset, endOffset)
+          .trim();
+        const precedingText = currentNode.textContent
+          ?.slice(0, startOffset)
+          .trim();
+        const succeedingText = currentNode.textContent
+          ?.slice(endOffset, currentNode.textContent.length)
+          .trim();
+
+        const span = document.createElement("span");
+        span.classList.add("highlight");
+        span.textContent = selectedText || "";
+
+        const fragment = document.createDocumentFragment();
+        if (precedingText)
+          fragment.appendChild(document.createTextNode(precedingText));
+
+        if (selectedText) fragment.appendChild(span);
+
+        if (succeedingText)
+          fragment.appendChild(document.createTextNode(succeedingText));
+
+        if (currentNode.parentNode) {
+          walker.currentNode = currentNode.parentNode;
+          currentNode.parentNode?.replaceChild(fragment, currentNode);
+        }
+      }
+      currentNode = walker.nextNode();
+    }
   }
 
   /**
@@ -55,7 +117,7 @@ export default class Marker {
    * Unmark or remove any marking or annotation from all selections.
    */
   unmarkAll() {
-    this.#marked.forEach((value, key) => this.unmark(key));
+    this.#marked.forEach((_, key) => this.unmark(key));
   }
 
   delete() {}
