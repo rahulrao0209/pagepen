fc455dd667aa2969ffcd54278275c9e9f83c7124
Rahul Rao <rahulrao0209@gmail.com>
2024-07-04 13:43:41 +0530

    Pagepen | Improve Webpack setup by introducing basic code-splitting and caching.

--- webpack.config.js
+++ webpack.config.js
@@ -39,8 +39,21 @@ module.exports = {
         extensions: ['.tsx', '.ts', '.js'],
     },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
+    optimization: {
+        moduleIds: 'deterministic',
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                }
+            }
+        }
+    }
 };
